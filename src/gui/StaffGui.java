c1e37e3c5991ed369bacb43a4fd534daa6c3d7c6
johnricketts <johnnri@gmail.com>
2016-05-31 15:09:42 +0200

    deleted gui classes

--- src/gui/StaffGui.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-/**
- * Created by John Ricketts on 2016-05-31.
- * GUI for
- */
-public class StaffGui {
-
-}
--- src/gui/VisitorGui.java
+++
@@ -1,7 +0,0 @@
-package gui;
-
-/**
- * Created by John on 2016-05-31.
- */
-public class VisitorGui {
-}
